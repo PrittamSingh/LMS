c57b3a2a53354791c0a7c0a1ff3070dd836493da
Prittam <prittamraj2018282@gmail.com>
2026-04-28 01:35:49 +0530

    updated mogoDB setup

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -7,13 +7,13 @@ export const clerkWebHooks = async (req, res) => {
     try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
-        await whook.verify(JSON.stringify(Request.body), {
+        await whook.verify(JSON.stringify(req.body), {
             "svix-id": req.headers["svix-id"], 
             "svix-timestamp": req.headers["svix-timestamp"],
-            "svix-signature": req.headers["svix-signature"]
-        })
+            "svix-signature": req.headers["svix-signature"],
+        });
 
-        const {data, type} = req.body
+        const {data, type} = req.body;
 
         switch (type) {
             case 'user.created': {
@@ -30,7 +30,7 @@ export const clerkWebHooks = async (req, res) => {
                 
             case 'user.updated' : {
                 const userData = {
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
                 }
